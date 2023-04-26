e76cd07f591a1ae5d9540b105f4446e4b4017f35
ahmed boulaouane <bouahmed530@gmail.com>
2023-04-26 12:50:47 +0100

    viewPort cree

--- gestion_facturation/src/App.jsx
+++ gestion_facturation/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Ajouter from './Ajouter';
 import AddContainer from './components/AddContainer';
 import AddContext from './contexts/AddContext';
+import ViewPort from './components/ViewPort';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
                 <Route index path='/devis' element={<></>}/>
                 <Route path='/facture' element={<Container/>}/>
                 <Route path='/ajouter' element={<Ajouter/>}/>
+                <Route path='/view' element={<ViewPort element={{ societe:{raison_s:"hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",contact:'dsfdfsdsfds',patente:'1555151',tel:'62266226',ICE:"fssdfdsfds",RC:"sqdqsdsqd"}, client:{id:15,raison_s:"byeee",adresse:'av tetouane, tanger',ICE:"fsdfsfs",LE:"s515sdf5s"},facture:{	N_facture:1,date_facture:'2015-1-12',date_echeance:'2015-1-12'}, commands:[], prixTotale:100000 }}/>}/>
                 <Route path='*' element={<Navigate to={"/devis"}/>}/>
               </Routes>
             </Container>
